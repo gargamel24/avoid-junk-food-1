73a5863fe94fdc201768df6497004a28b4b66942
gargamel24 <81331003+gargamel24@users.noreply.github.com>
2021-04-21 17:31:47 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,66 @@
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    fish.setImage(fishLeftImg)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    fish.setImage(fishRightImg)
+})
+function makeEnemy () {
+    evilburger = sprites.create(img`
+        . . . . c c c b b b b b . . . . 
+        . . c c b 4 4 4 4 4 4 b b b . . 
+        . c c 4 4 4 4 4 5 4 4 4 4 b c . 
+        . e 4 4 4 4 4 4 4 4 4 5 4 4 e . 
+        e b 4 5 4 4 5 4 4 4 4 4 4 4 b c 
+        e b 4 4 4 4 4 4 4 4 4 4 5 4 4 e 
+        e b b 4 4 4 4 4 4 4 4 4 4 4 b e 
+        . e b 4 4 4 4 4 5 4 4 4 4 b e . 
+        8 7 e e b 4 4 4 4 4 4 b e e 6 8 
+        8 7 2 e e e e e e e e e e 2 7 8 
+        e 6 6 2 2 2 2 2 2 2 2 2 2 6 c e 
+        e c 6 7 6 6 7 7 7 6 6 7 6 c c e 
+        e b e 8 8 c c 8 8 c c c 8 e b e 
+        e e b e c c e e e e e c e b e e 
+        . e e b b 4 4 4 4 4 4 4 4 e e . 
+        . . . c c c c c e e e e e . . . 
+        `, SpriteKind.Enemy)
+    evilburger.vx = 40
+    evilburger.vy = 60
+    evilburger.y = 0
+    evilburger.x = randint(0, 160)
+    evilburger.setFlag(SpriteFlag.BounceOnWall, true)
+evilburger.ay = 70
+
+
+
+
+
+}
+let evilburger: Sprite = null
+let fish: Sprite = null
+let fishRightImg: Image = null
+let fishLeftImg: Image = null
+fishLeftImg = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c c c c c . . . 
+    . . . . . . c 5 5 5 5 5 c c . . 
+    . . . . . c 5 5 5 5 5 5 5 5 c . 
+    . . . . c b b b b b b 5 5 5 c . 
+    . . . . c b b b b 1 b b c c . . 
+    . . . . c 1 1 b b 1 1 1 c . . . 
+    . . . c 1 1 1 1 b 1 1 1 c . . . 
+    . . . c 1 1 1 1 b 1 1 1 b b c c 
+    . . c c d 1 1 1 b 1 b 1 5 5 5 c 
+    . . c c d 1 c 1 1 1 b 1 b b 5 c 
+    . c c d d 1 1 1 1 1 b 1 f b 5 c 
+    f d d d 1 1 1 1 1 b b b f . c c 
+    f f f f f 1 1 1 b b 5 5 5 f . . 
+    . . . . . f f f 5 5 5 5 5 f . . 
+    . . . . . . . . f f f f f f . . 
+    `
+fishRightImg = fishLeftImg.clone()
+fishRightImg.flipX()
+fish = sprites.create(fishLeftImg, SpriteKind.Player)
+fish.y = 110
+controller.moveSprite(fish, 100, 0)
+fish.setStayInScreen(true)
+makeEnemy()
